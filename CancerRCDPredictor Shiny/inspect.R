924b616001bb21782fc72026f18773c25a775bfc
Enrique Medina Acosta <quique@uenf.br>
2026-07-06 00:50:52 -0300

    Delete CancerRCDPredictor Shiny directory

--- CancerRCDPredictor Shiny/inspect.R
+++
@@ -1,3 +0,0 @@
-library(readxl)
-df <- read_excel("Supplementary Table_v036_EMA.xlsx", sheet = "Table S16")
-print(head(df))
--- CancerRCDPredictor Shiny/start code (only for me).R
+++
@@ -1,2 +0,0 @@
-setwd("~/students/aluno0549-6/PHASE_III/PHASE_III_Megarun_5_0_complete_ZIMA_Suit_Generator_final/PHASE_IV_CancerRCDShiny")
-shiny::runApp("~/students/aluno0549-6/PHASE_III/PHASE_III_Megarun_5_0_complete_ZIMA_Suit_Generator_final/PHASE_IV_CancerRCDShiny")
